763c403a21c97a29ab7d164f62e165d8407525d5
Rebeca <mebuffbeka@gmail.com>
2024-05-21 08:40:07 -0300

    exercicio

--- JavaScript-aula/exercicio/script.js
+++ JavaScript-aula/exercicio/script.js
@@ -4,9 +4,8 @@ let campoA = document.querySelector('input#campoA');
 let campoB = document.querySelector('input#campoB'); 
 
 // Primeiro pegar o evento do click
-form.addEventListener('submit'), function(e) {
+form.addEventListener('submit', e => {
     e.preventDefault()
-    console.log('clicou');
-} 
+    console.log(form);
+}) 
 
-console.log(form);
