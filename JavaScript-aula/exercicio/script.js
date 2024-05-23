aa14c00dd9bea3d02599fa0d826d7d8a338c8fde
Rebeca <mebuffbeka@gmail.com>
2024-05-23 09:50:03 -0300

    Finalizando atividade js-html

--- JavaScript-aula/exercicio/script.js
+++ JavaScript-aula/exercicio/script.js
@@ -2,10 +2,37 @@
 const form = document.querySelector('#form');
 let campoA = document.querySelector('input#campoA');
 let campoB = document.querySelector('input#campoB'); 
+let result = document.getElementById('result');
 
-// Primeiro pegar o evento do click
 form.addEventListener('submit', e => {
     e.preventDefault()
-    console.log(form);
+    
+    const resultadoAnaliseNumeros = analisaNumeros();
+    // analisaNumeros();
+    addTexto(resultadoAnaliseNumeros);
 }) 
+
+function analisaNumeros(){
+    const campoAMaior = `<b>${campoA.value}</b> é maior que ${campoB.value}`;
+    const campoBMaior = `<b>${campoB.value}</b> é maior que ${campoA.value}`;
+    const camposDeMesmoValor = `<b>${campoA.value}</b> e <b>${campoB.value}</b> tem o mesmo valor`;
+
+    if(campoA.value > campoB.value){
+        return campoAMaior;
+    } else if (campoA.value < campoB.value){
+        return campoBMaior;
+    } else {
+        return camposDeMesmoValor;
+    }
+
+    //Operador ternário com apenas 2 condições
+    //campoA.value > campoB.value ? console.log(campoAMaior) : console.log(campoBMaior);
+}
+
+function addTexto(resultado) {
+    result.innerHTML = resultado;
+    
+}
+
+
 
