aa29bab66cd45bde3562927f2ac5d313590c6207
Rebeca Oliveira <mebuffbeka@gmail.com>
2024-06-14 09:24:45 -0300

    atualizando commit

--- projeto2/js/main.js
+++ projeto2/js/main.js
@@ -3,6 +3,8 @@ const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando">'
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado">'
 const atividades = [];
 const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
 
 let linhas = '';
 
@@ -42,7 +44,7 @@ function atualizaMediaFinal() {
   const mediaFinal = calculaMediaFinal();
 
   document.getElementById('media-final-valor').innerHTML = mediaFinal;
-  document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? 'Aprovado' : 'Reprovado';
+  document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? spanAprovado : spanReprovado;
 }
 
 function calculaMediaFinal(){
