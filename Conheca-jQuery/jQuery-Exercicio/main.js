d8f260411e89eb7169196b699df81828e0e1a755
RebecaTI <mebuffbeka@gmail.com>
2024-07-23 10:35:11 -0300

    Exercicio finalizado

--- Conheca-jQuery/jQuery-Exercicio/main.js
+++ Conheca-jQuery/jQuery-Exercicio/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('button').click( (e) => {
     e.preventDefault();
     $('ul').append(`<li>${$('input').val()} </li>`)
+    $('input').val('');
   });
 
   $('ul').on('click', 'li', function (e) {
