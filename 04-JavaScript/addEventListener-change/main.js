2569c140b8e80b358e11d6e859844f03230bd958
RebecaTI <mebuffbeka@gmail.com>
2024-12-14 12:33:01 -0300

    tentando resolver problemas com o meu git

--- 04-JavaScript/addEventListener-change/main.js
+++
@@ -1,7 +0,0 @@
-const selectElement = document.querySelector('.ice-cream');
-const result = document.querySelector('.result');
-
-selectElement.addEventListener('change', (e) => {
-    result.textContent = `You like ${e.target.value}`;
-})
-
