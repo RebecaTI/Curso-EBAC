1f09bc179a4dae36e65e65a9152519aec13f6a60
RebecaTI <mebuffbeka@gmail.com>
2024-12-26 18:34:23 -0300

    sass

---
+++ 04-JavaScript/funtion.js
@@ -0,0 +1,10 @@
+function descobrirIdade(anoNascimento){
+    return 2024 - anoNascimento;
+}
+console.log(descobrirIdade(1995));
+
+function somarNumeros(numA, numB, numC){
+    return numA + numB + numC;
+}
+console.log(somarNumeros(10,20,30));
+
